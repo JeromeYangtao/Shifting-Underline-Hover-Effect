0cd03c75d05b461482de3b105c7c4e34f11bd6c8
your name <your_email@youremail.com>
2017-04-06 19:07:59 +0800

    视窗大小改变时的调整

--- index.js
+++ index.js
@@ -7,12 +7,41 @@ for (var i = 0; i < links.length; i++) {
 }
 
 function mouseenterFunc() {
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].parentNode.classList.contains('active')) {
-            links[i].parentNode.classList.remove('active');
+    if (!this.parentNode.classList.contains('active')) {
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].parentNode.classList.contains('active')) {
+                links[i].parentNode.classList.remove('active');
+            }
+            links[i].style.opacity = '0.25';
         }
-        links[i].style.opacity = '0.25';
+
+        this.parentNode.classList.add('active');
+        this.style.opacity = '1';
+
+        var width = this.getBoundingClientRect().width;
+        var height = this.getBoundingClientRect().height;
+        var left = this.getBoundingClientRect().left + window.screenX;
+        var top = this.getBoundingClientRect().top + window.screenX;
+        var color = colors[Math.floor(Math.random() * colors.length)];
+
+        target.style.width = `${width}px`;
+        target.style.height = `${height}px`;
+        target.style.left = `${left}px`;
+        target.style.top = `${top}px`;
+        target.style.borderColor = color;
+        target.style.transform = 'none';
     }
-    this.parentNode.classList.add('active');
-    this.style.opacity = '1';
+}
+
+// 视窗大小改变时
+window.addEventListener('resize', resizeFunc);
+
+function resizeFunc() {
+    var active = document.querySelector('.nav ul li.active');
+
+    var left = active.getBoundingClientRect().left + window.screenX;
+    var top = active.getBoundingClientRect().top + window.screenX;
+
+    target.style.left = `${left}px`;
+    target.style.top = `${top}px`;
 }
